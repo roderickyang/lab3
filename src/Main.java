3b47b0eb849b96a331e2bf289373222eb70c88ea
Roderick Yang <zy7@illinois.edu>
2018-02-07 18:48:19 -0600

    initialization.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,36 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
+/**
+ * Some description.
+ */
+
 public class Main {
+
+    /**
+     * Retrieve contents from a URL and return them as a string.
+     *
+     * @param url url to retrieve contents from
+     * @return the contents from the url as a string, or an empty string on error
+     */
+
+    public static String urlToString(final String url) {
+        Scanner urlScanner;
+        try {
+            urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+        } catch (IOException e) {
+            return "";
+        }
+        String contents = urlScanner.useDelimiter("\\A").next();
+        urlScanner.close();
+        return contents;
+    }
+
+    /**
+     * description.
+     * @param args
+     */
     public static void main(String[] args) {
         System.out.println("here");
     }
